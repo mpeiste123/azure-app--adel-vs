1d0b967eaf503134e9021dd0999d19831e3e7b8e
mpeiste123 <adel.sattari@gmail.com>
2024-04-28 20:48:22 +0200

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace azure_app__trev_vs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
